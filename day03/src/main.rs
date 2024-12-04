f40f4377b21d03c91ead80b0aca73d625ab1bf6e
Kyle Gretchev <kgretchev@gmail.com>
2024-12-03 21:45:17 -0500

    Greatly simplified day 03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -3,8 +3,9 @@ use std::fs;
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{char, i64},
+    character::complete::{anychar, char, i64},
     combinator::map,
+    multi::many_till,
     sequence::tuple,
     Finish, IResult,
 };
@@ -24,16 +25,7 @@ fn part1() -> anyhow::Result<i64> {
     let mut input = raw_input.as_str();
     let mut sum = 0;
 
-    loop {
-        if input.is_empty() {
-            break;
-        }
-
-        let Ok((remaining, (left, right))) = mul_parser_part_1(input).finish() else {
-            input = &input[1..];
-            continue;
-        };
-
+    while let Some((remaining, (left, right))) = mul_parser_part_1(input).finish().ok() {
         sum += left * right;
         input = remaining;
     }
@@ -47,16 +39,7 @@ fn part2() -> anyhow::Result<i64> {
     let mut sum = 0;
     let mut enabled = true;
 
-    loop {
-        if input.is_empty() {
-            break;
-        }
-
-        let Ok((remaining, instruction)) = combo_parser(input).finish() else {
-            input = &input[1..];
-            continue;
-        };
-
+    while let Some((remaining, instruction)) = combo_parser(input).finish().ok() {
         match instruction {
             Instruction::Do => enabled = true,
             Instruction::DoNot => enabled = false,
@@ -78,7 +61,8 @@ enum Instruction {
 
 fn mul_parser_part_1(input: &str) -> IResult<&str, (i64, i64)> {
     let parser = tuple((tag("mul"), char('('), i64, char(','), i64, char(')')));
-    map(parser, |(_, _, left, _, right, _)| (left, right))(input)
+    let many_parser = many_till(anychar, parser);
+    map(many_parser, |(_, (_, _, left, _, right, _))| (left, right))(input)
 }
 
 fn mul_parser(input: &str) -> IResult<&str, Instruction> {
@@ -89,15 +73,17 @@ fn mul_parser(input: &str) -> IResult<&str, Instruction> {
 }
 
 fn do_parser(input: &str) -> IResult<&str, Instruction> {
-    let parser = tuple((tag("do"), char('('), char(')')));
+    let parser = tag("do()");
     map(parser, |_| Instruction::Do)(input)
 }
 
 fn do_not_parser(input: &str) -> IResult<&str, Instruction> {
-    let parser = tuple((tag("don't"), char('('), char(')')));
+    let parser = tag("don't()");
     map(parser, |_| Instruction::DoNot)(input)
 }
 
 fn combo_parser(input: &str) -> IResult<&str, Instruction> {
-    alt((mul_parser, do_parser, do_not_parser))(input)
+    let parser = alt((mul_parser, do_parser, do_not_parser));
+    let many_parser = many_till(anychar, parser);
+    map(many_parser, |(_, instruction)| instruction)(input)
 }
