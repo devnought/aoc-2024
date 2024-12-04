e1f8216ad8ecfe8c3e163860d65b3d7e453bafd8
Kyle Gretchev <kgretchev@gmail.com>
2024-12-04 14:16:43 -0500

    Factored out common code

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::{self};
 
+use anyhow::anyhow;
 use grid::Grid;
 
 fn main() -> anyhow::Result<()> {
@@ -14,37 +15,8 @@ fn main() -> anyhow::Result<()> {
 
 fn part1() -> anyhow::Result<usize> {
     let raw_data = fs::read_to_string("./input.txt")?;
-    let grid = {
-        let width = {
-            let Some(row) = raw_data.lines().map(|line| line.trim()).next() else {
-                return Ok(0);
-            };
-
-            row.len()
-        };
-
-        let data = raw_data
-            .chars()
-            .filter(|c| !c.is_whitespace())
-            .collect::<Vec<_>>();
-
-        Grid::from_vec(data, width)
-    };
-
-    let iter = (0..grid.rows())
-        .flat_map(|row_index| {
-            grid.iter_row(row_index)
-                .enumerate()
-                .map(move |(col_index, character)| ((col_index, row_index), character))
-        })
-        .filter_map(|(coords, character)| {
-            if *character == 'X' {
-                Some(coords)
-            } else {
-                None
-            }
-        });
-
+    let grid = build_grid(raw_data)?;
+    let iter = build_main_iter(&grid, 'X');
     let sum = iter.map(|coord| xmas_word_count(&grid, coord)).sum();
 
     Ok(sum)
@@ -52,40 +24,47 @@ fn part1() -> anyhow::Result<usize> {
 
 fn part2() -> anyhow::Result<usize> {
     let raw_data = fs::read_to_string("./input.txt")?;
-    let grid = {
-        let width = {
-            let Some(row) = raw_data.lines().map(|line| line.trim()).next() else {
-                return Ok(0);
-            };
+    let grid = build_grid(raw_data)?;
+    let iter = build_main_iter(&grid, 'A');
+    let sum = iter.map(|coord| crossed_mas(&grid, coord)).sum();
 
-            row.len()
+    Ok(sum)
+}
+
+fn build_grid(input: String) -> anyhow::Result<Grid<char>> {
+    let width = {
+        let Some(row) = input.lines().map(|line| line.trim()).next() else {
+            return Err(anyhow!("Cannot build grid from empty dataset"));
         };
 
-        let data = raw_data
-            .chars()
-            .filter(|c| !c.is_whitespace())
-            .collect::<Vec<_>>();
-
-        Grid::from_vec(data, width)
+        row.len()
     };
 
-    let iter = (0..grid.rows())
+    let data = input
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<Vec<_>>();
+
+    Ok(Grid::from_vec(data, width))
+}
+
+fn build_main_iter(
+    grid: &Grid<char>,
+    character_origin: char,
+) -> impl Iterator<Item = (usize, usize)> + use<'_> {
+    (0..grid.rows())
         .flat_map(|row_index| {
             grid.iter_row(row_index)
                 .enumerate()
                 .map(move |(col_index, character)| ((col_index, row_index), character))
         })
-        .filter_map(|(coords, character)| {
-            if *character == 'A' {
+        .filter_map(move |(coords, character)| {
+            if *character == character_origin {
                 Some(coords)
             } else {
                 None
             }
-        });
-
-    let sum = iter.map(|coord| crossed_mas(&grid, coord)).sum();
-
-    Ok(sum)
+        })
 }
 
 fn xmas_word_count(grid: &Grid<char>, (x, y): (usize, usize)) -> usize {
@@ -95,8 +74,8 @@ fn xmas_word_count(grid: &Grid<char>, (x, y): (usize, usize)) -> usize {
 
     let coords = directions.map(|(dir_x, dir_y)| {
         [
-            (x as isize + (dir_x * 0), y as isize + (dir_y * 0)),
-            (x as isize + (dir_x * 1), y as isize + (dir_y * 1)),
+            (x as isize, y as isize),
+            (x as isize + dir_x, y as isize + dir_y),
             (x as isize + (dir_x * 2), y as isize + (dir_y * 2)),
             (x as isize + (dir_x * 3), y as isize + (dir_y * 3)),
         ]
@@ -127,9 +106,9 @@ fn crossed_mas(grid: &Grid<char>, (x, y): (usize, usize)) -> usize {
 
     let coords = directions.map(|(dir_x, dir_y)| {
         [
-            (x as isize + (dir_x * -1), y as isize + (dir_y * -1)),
-            (x as isize + (dir_x * 0), y as isize + (dir_y * 0)),
-            (x as isize + (dir_x * 1), y as isize + (dir_y * 1)),
+            (x as isize + -dir_x, y as isize + -dir_y),
+            (x as isize, y as isize),
+            (x as isize + dir_x, y as isize + dir_y),
         ]
     });
 
